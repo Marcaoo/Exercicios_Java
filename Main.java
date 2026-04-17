727248fb74898c146072b028ac419e6c926f4110
Marcão <o_marcaoo@outlook.com>
2026-04-17 10:02:53 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+//Exercício 1 -
+
 package curso_programacaoo;
 
 import java.util.Scanner;
@@ -5,20 +7,20 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		int hora;
+		int hora; // define a variável hora
 		
 		Scanner sc = new Scanner(System.in);
 		hora = sc.nextInt();
 		
-		if(hora < 12) {
+		if(hora < 12) {	// verifica se é demanhã
 			System.out.println("Bom dia");
 		}
 		else {
-			if(hora < 18) {
+			if(hora < 18) {	// verifica se é detarde
 				System.out.println("Boa tarde");
 			}
 			else {
-				System.out.println("Boa noite");
+				System.out.println("Boa noite"); // verifica se é anoite
 			}
 			
 		}
